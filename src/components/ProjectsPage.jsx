31fc0e2c703205b111bd6b25943c597fcc7ada92
pardeek <katiepardee1@gmail.com>
2025-02-17 16:07:45 -0800

    fixed heading heirarchy

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -5,7 +5,7 @@ const ProjectsPage = () => {
         <div className='content-width'>
             <div className="col">
                 <h2 className='title-font'><span className='text-gradient'>Projects</span> Portfolio</h2>
-                <h2 className='page-subtitle'>React | Astro | Wordpress</h2>
+                <h3 className='page-subtitle subtitle'>React | Astro | Wordpress</h3>
                 <p className='page-description'>Proficient in HTML, CSS, JavaScript (ES6), and React.</p>                
             </div>
             <ProjectCarousel client:load />
--- src/components/SkillsPage.jsx
+++ src/components/SkillsPage.jsx
@@ -5,7 +5,7 @@ const SkillsPage = () => {
         <div className='content-width'>
             <div className='col'>
                 <h2 className='title-font'>Let's talk <span className='text-gradient'>skills</span>.</h2>
-                <h2>4+ years in Web Development</h2>
+                <h3 className='subtitle'>4+ years in Web Development</h3>
                 <p>Proficient in HTML, CSS, JavaScript (ES6), and React.</p>                
             </div>
             <SkillsCards client:load />
